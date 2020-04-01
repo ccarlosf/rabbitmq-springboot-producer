02039290123c66467a770cae1f8139c7b378a3b5
ccarlos <2238513100@qq.com>
2020-04-01 22:33:38 +0800

    RabbitMQ与SpringBoot2.0整合 生成端

---
+++ src/main/java/com/ccarlos/springboot/MainConfig.java
@@ -0,0 +1,10 @@
+package com.ccarlos.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.ccarlos.springboot.*"})
+public class MainConfig {
+
+}
---
+++ src/test/java/com/ccarlos/springboot/RabbitmqSpringbootProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.springboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringbootProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
